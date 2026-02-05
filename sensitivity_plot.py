a368e96af843c4ebc299b301f3ae9a96c7757970
tim-lemm <tim3lem@gmail.com>
2026-02-05 11:34:33 +0100

    plot for ta_due and ta_sto

--- sensitivity_plot.py
+++ sensitivity_plot.py
@@ -1,23 +1,27 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from od_matrix_generator import generate_od_df
 
-CURRENT_DIR = "/Users/tristan.lemoalle/Documents/Thèse/Code/code_these/"
+CURRENT_DIR = ""
 
-df_results = pd.read_json(f"{CURRENT_DIR}output/sensitivity_due.json")
-print(type(df_results))
+df_results_due = pd.read_json(f"{CURRENT_DIR}output/sensitivity_due.json")
+df_results_sto = pd.read_json(f"{CURRENT_DIR}output/sensitivity_sto.json")
+df_demand = pd.read_json(f"{CURRENT_DIR}output/demand.json")
 
 list_od_scenarios=['1OD','2OD',"RANDOM_OD"]
 list_max_demand = [500,1000,2000,3000,4000,5000]
+
+### Plots for ta_due
+## Plotting convergence
+
 list_algorithm = ['bfw', 'fw', 'msa']
-
-
 fig, axes = plt.subplots(3,3, figsize=(30,20))
 j = 0
 for algorithm in list_algorithm:
     i = 0
     for scenario in list_od_scenarios:
         for max_demand in list_max_demand:
-            pd.DataFrame.from_dict(df_results[f"{algorithm}_{scenario}_{max_demand}"]['convergence']).plot.line(x='iteration', y='rgap', ax=axes[i,j], label=max_demand)
+            pd.DataFrame.from_dict(df_results_due[f"{algorithm}_{scenario}_{max_demand}"]['convergence']).plot.line(x='iteration', y='rgap', ax=axes[i,j], label=max_demand)
         axes[i,j].set_title(f"{algorithm}_{scenario}")
         axes[i,j].set_xlabel("")
         axes[i,j].set_ylabel("")
@@ -29,23 +33,70 @@ axes[1,0].set_ylabel("rgap")
 plt.tight_layout()
 plt.show()
 
-fig, axes = plt.subplots(3,3, figsize=(30,20)) #TODO: plot bar plot for total travel time for every scenario, algorithm and max_demand
+## Plotting total travel time vs total demand
+fig, axes = plt.subplots(3,3, figsize=(30,20))
 j = 0
-#for algorithm in list_algorithm:
-#    i = 0
-#    for scenario in list_od_scenarios:
-#        for max_demand in list_max_demand:
-#            df_results[f"{algorithm}_{scenario}_{max_demand}"].plot.bar(x='total_travel_time', ax=axes[i,j], label=max_demand)
-#        axes[i,j].set_title(f"{algorithm}_{scenario}")
-#        axes[i,j].set_xlabel("")
-#        axes[i,j].set_ylabel("")
-#        axes[i,j].grid(alpha=0.5)
-#        i +=1
-#    j+=1
-#axes[2,1].set_xlabel("Iteration")
-#axes[1,0].set_ylabel("rgap")
-#plt.tight_layout()
-#plt.show()
+for algorithm in list_algorithm:
+   i = 0
+   for scenario in list_od_scenarios:
+       x = []
+       y = []
+       for max_demand in list_max_demand:
+           y.append(df_results_due[f"{algorithm}_{scenario}_{max_demand}"]["total_travel_time"])
+           x.append(df_demand[(df_demand["scenario"] == scenario) & (df_demand["max_demand"] == max_demand)]["total_demand"].values[0])
+       axes[i,j].plot(x, y, marker='o')
+       axes[i,j].set_title(f"{algorithm}_{scenario}")
+       axes[i,j].set_xlabel("")
+       axes[i,j].set_ylabel("")
+       axes[i,j].grid(alpha=0.5)
+       i +=1
+   j+=1
+axes[2,1].set_xlabel("Total Demand")
+axes[1,0].set_ylabel("Total Travel Time")
+plt.tight_layout()
+plt.show()
 
-#TODO: create similar plots for sensitivity of stochastic assignment
+### Plot for ta_sto
+list_algorithm = ['bfsle', 'lp']
+list_max_route = [1,2,3,4,5]
+fig, axes = plt.subplots(3,2, figsize=(30,20))
+j = 0
+for algorithm in list_algorithm:
+   i = 0
+   for scenario in list_od_scenarios:
+       x = []
+       y_1 = []
+       y_2 = []
+       y_3 = []
+       y_4 = []
+       y_5 = []
+       for max_demand in list_max_demand:
+           x.append(df_demand[(df_demand["scenario"] == scenario) & (df_demand["max_demand"] == max_demand)]["total_demand"].values[0])
+           y_1.append(df_results_sto[f"{algorithm}_1_{scenario}_{max_demand}"]["total_travel_time"])
+           y_2.append(df_results_sto[f"{algorithm}_2_{scenario}_{max_demand}"]["total_travel_time"])
+           y_3.append(df_results_sto[f"{algorithm}_3_{scenario}_{max_demand}"]["total_travel_time"])
+           y_4.append(df_results_sto[f"{algorithm}_4_{scenario}_{max_demand}"]["total_travel_time"])
+           y_5.append(df_results_sto[f"{algorithm}_5_{scenario}_{max_demand}"]["total_travel_time"])
+       axes[i,j].plot(x, y_1, marker='o', color='blue', label='1')
+       axes[i,j].plot(x, y_2, marker='o', color='red', label='2')
+       axes[i,j].plot(x, y_3, marker='o', color='green', label='3')
+       axes[i, j].plot(x, y_4, marker='o', color='yellow', label='4')
+       axes[i, j].plot(x, y_5, marker='o', color='pink', label='5')
+       axes[i,j].legend(title="Number of max_routes")
+       axes[i,j].set_title(f"{algorithm}_{scenario}")
+       axes[i,j].set_xlabel("")
+       axes[i,j].set_ylabel("")
+       axes[i,j].grid(alpha=0.5)
+       i +=1
+   j+=1
+axes[2,1].set_xlabel("Total Demand")
+axes[1,0].set_ylabel("Total Travel Time")
+plt.tight_layout()
+plt.show()
+
+
 #TODO: create similar plots for sensitivity of mode choice
+
+
+
+
--- sensitivity_ta.py
+++ sensitivity_ta.py
@@ -6,32 +6,78 @@ from plotting import *
 from od_matrix_generator import *
 from eaquilibrea_interface import *
 from network_processing import *
-from src.utils_sta import ta_due
+from src.utils_sta import ta_due, ta_stochastic
 
-CURRENT_DIR = "/Users/tristan.lemoalle/Documents/Thèse/Code/code_these/"
+CURRENT_DIR = ""
 
 edge_df, node_df = import_network(CURRENT_DIR + "data/edges_small_grid_2.csv", CURRENT_DIR + "data/nodes_small_grid_2.csv")
 
 list_od_scenarios=['1OD','2OD',"RANDOM_OD"]
 list_max_demand = [500,1000,2000,3000,4000,5000]
+
+### Processing sensitivity analysis on ta_due
 list_algorithm = ['bfw', 'fw', 'msa']
 
+# df_results = pd.DataFrame()
+# for algorithm in list_algorithm:
+#     for scenario in list_od_scenarios:
+#         for max_demand in list_max_demand:
+#             name = f"{algorithm}_{scenario}_{max_demand}"
+#             od_df = generate_od_df(17, od_scenario=scenario, max_demand=max_demand)
+#             od_df_eaq = convert_to_eaquilibrae_od_matrix(od_df)
+#             results_ta_due = ta_due(edge_df,
+#                                     od_df_eaq,
+#                                     algorithm=algorithm,
+#                                     max_iter=500,
+#                                     tolerance=1e-4,
+#                                     time_field='free_flow_time_car',
+#                                     cost_field='free_flow_time_car',
+#                                     capacity_field='capacity_cars',
+#                                     verbose=True)
+#             df_results[name] = results_ta_due
+
+# df_results.to_json(f"{CURRENT_DIR}output/sensitivity_due.json")
+
+###processing sensitivity analysis on ta_sto
+
+list_algorithm = ['bfsle', 'lp']
+list_max_route = [1,2,3,4,5]
 df_results = pd.DataFrame()
 for algorithm in list_algorithm:
     for scenario in list_od_scenarios:
         for max_demand in list_max_demand:
-            name = f"{algorithm}_{scenario}_{max_demand}"
+
             od_df = generate_od_df(17, od_scenario=scenario, max_demand=max_demand)
             od_df_eaq = convert_to_eaquilibrae_od_matrix(od_df)
-            results_ta_due = ta_due(edge_df,
-                                    od_df_eaq,
-                                    algorithm=algorithm,
-                                    max_iter=500,
-                                    tolerance=1e-4,
-                                    time_field='free_flow_time_car',
-                                    cost_field='free_flow_time_car',
-                                    capacity_field='capacity_cars',
-                                    verbose=True)
-            df_results[name] = results_ta_due
+            for max_route in list_max_route:
+                name = f"{algorithm}_{max_route}_{scenario}_{max_demand}"
+                results_ta_sto = ta_stochastic(edge_df,
+                                               od_df_eaq,
+                                               mode='bikes',
+                                               time_field='free_flow_time_bike',
+                                               cost_field='free_flow_time_bike',  ### LENGTH OR LENGTH_BI?
+                                               algorithm=algorithm,
+                                               max_routes=max_route,
+                                               capacity_field='capacity_bikes',
+                                               verbose=True)
+                df_results[name] = results_ta_sto
 
-df_results.to_json(f"{CURRENT_DIR}output/sensitivity_due.json")
+df_results.to_json(f"{CURRENT_DIR}output/sensitivity_sto.json")
+
+
+### getting total demand from the different scenario of OD
+
+# df_demand = pd.DataFrame(columns=['scenario','max_demand','total_demand'])
+#
+# rows_list = []
+# for scenario in list_od_scenarios:
+#     for max_demand in list_max_demand:
+#         od_matrix = generate_od_df(17, scenario, max_demand=max_demand)
+#         rows_list.append({
+#             'scenario': scenario,
+#             'max_demand': max_demand,
+#             'total_demand': od_matrix.sum().sum()
+#         })
+# df_demand = pd.DataFrame(rows_list)
+#
+# df_demand.to_json(f"{CURRENT_DIR}output/demand.json")
